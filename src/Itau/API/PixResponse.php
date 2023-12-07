9932e1790dc383b109ed923c286dc3fc486f1518
Leandro Ferreira Marcelli <suporte@integracaosistema.com.br>
2023-12-07 14:40:29 -0300

    response pix

--- src/Itau/API/PixResponse.php
+++ src/Itau/API/PixResponse.php
@@ -4,5 +4,16 @@ namespace Itau\API;
 
 class PixResponse extends BaseResponse
 {
-    
+    private $txid;
+    private $pixCopiaECola;
+
+    public function Txid()
+    {
+        return $this->txid;
+    }
+
+    public function getPixCopiaECola()
+    {
+        return $this->pixCopiaECola;
+    }
 }
