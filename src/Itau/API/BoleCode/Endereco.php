2203884513bf4adee4676fa3cd412358d1d85a47
Leandro Ferreira Marcelli <suporte@integracaosistema.com.br>
2023-12-29 10:40:43 -0300

    Limpeza dos textos

--- src/Itau/API/BoleCode/Endereco.php
+++ src/Itau/API/BoleCode/Endereco.php
@@ -21,14 +21,27 @@ class Endereco implements JsonSerializable
         string $rua, string $numero, string $complemento, string $bairro, string $cidade, string $uf, string $cep
     ): self
     {
-        $this->nome_logradouro = mb_substr($rua, 0, 45);
-        $this->nome_bairro = mb_substr($bairro, 0, 15);
-        $this->nome_cidade = mb_substr($cidade, 0, 20);
-        $this->sigla_UF = mb_substr($uf, 0, 2);
+        $this->nome_logradouro = $this->textClear($rua, 45);
+        $this->nome_bairro = $this->textClear($bairro, 15);
+        $this->nome_cidade = $this->textClear($cidade, 20);
+        $this->sigla_UF = $this->textClear($uf, 2);
         $this->numero_CEP = preg_replace("/[^0-9]/", "", $cep);
-        $this->complemento = mb_substr($complemento, 0, 10);
-        $this->numero = $numero;
+        $this->complemento = $this->textClear($complemento, 10);
+        $this->numero = $this->textClear($numero, 10);
 
         return $this;
+    }
+
+    public function textClear(?string $value, int $count): ?string
+    {
+        if(empty($value)){
+            return null;
+        }
+        return mb_substr($this->textClearSpecialChar($value), 0, $count);
+    }
+
+    public function textClearSpecialChar(string $value): ?string
+    {
+        return preg_replace("/[^\w]/", "", $value);
     }
 }
