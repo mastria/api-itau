dbd039633bca47cf9e186c3d24864d0f54a3e351
Leandro Ferreira Marcelli <suporte@integracaosistema.com.br>
2024-12-11 17:02:15 -0300

    Debug

--- src/Itau/API/ItauCertificate.php
+++ src/Itau/API/ItauCertificate.php
@@ -21,11 +21,16 @@ class ItauCertificate
 
     public function renovarCertificado(Itau $credentials, string $certificadoCSR)
     {
+        echo "<h1>API</h1>";
+        var_dump($credentials->getAuthorizationToken());
         if (!$credentials->getAuthorizationToken()) {
+            echo '<hr>';
+            var_dump($credentials);
             new Request($credentials);
         }
         $token = $credentials->getAuthorizationToken();
-
+        echo '<hr>';
+        var_dump($token);
         return $this->executeRequest($token, $certificadoCSR);
     }
 
